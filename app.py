e3aaa39f16e442b7d670f2d85c286309a23b9ad7
JacoSeth <48956978+JacoSeth@users.noreply.github.com>
2020-01-28 08:39:35 -0700

    Add files via upload
    updated so app works w/ flask, minor changes to logic.js should allow me to grab info from app routes

---
+++ app.py
@@ -0,0 +1,298 @@
+# Imports
+from flask import (Flask, render_template)
+# import cd_sd_hd_county_counts 
+# from cd_sd_hd_county_counts import cd_total_accounts, sd_total_accounts, hd_total_accounts, county_total_accounts, cd_total_members, sd_total_members, hd_total_members, county_total_members, cd_total_unverified, sd_total_unverified, hd_total_unverified, county_total_unverified, cd_total_nonmembers, sd_total_nonmembers, hd_total_nonmembers, county_total_nonmembers 
+import pandas as pd
+coded = pd.read_csv('/Users/sethjacobson/Desktop/COLOGOPCL/admin_exports_by_date/CR-admin-export_1.26.2020.csv')
+coded_df = pd.DataFrame(coded)
+
+# Create the App
+
+app = Flask(__name__)
+
+# Home route
+@app.route("/")
+def show_map():
+     return render_template("index.html")
+
+
+@app.route("/user_data")
+def get_user_data():
+    coded_export = coded_df[['longitude', 'latitude','county', 'cd', 'sd', 'hd', 'roles',]]
+    return coded_export.to_json(orient='records')
+
+# cd_routes
+
+@app.route("/cd_1")
+def get_cd1_counts():
+    # Total Subscribers first
+    x_district = coded_df['cd'] == 1
+    x_district_coded = coded_df[x_district]
+    x_district_subscribers_count = coded_df[x_district].shape[0]
+    # Next, do Total Members
+    x_district_members_verified_count = x_district_coded['roles'] == 'member,verified'
+    x_district_verified_members_count = x_district_coded['roles'] == 'verified,member'
+    x_district_m_v = x_district_coded[x_district_members_verified_count]
+    x_district_v_m = x_district_coded[x_district_verified_members_count]
+    x_district_total_members = x_district_m_v.shape[0] + x_district_v_m.shape[0]
+    # Then, do Unverifieds
+    x_district_unverified = x_district_coded['verified'] == 'False'
+    xd_uv_count = x_district_coded[x_district_unverified].shape[0]
+    # xd_total_unverifieds = xd_uv.shape[0] + xd_uv_m.shape[0] + xd_m_uv.shape[0] + xd_uv_nm.shape[0] 
+    # Last, do Nonmembers
+    x_district_nonmembers_verified = x_district_coded['roles'] == 'nonmember,verified'
+    x_district_verified_nonmembers = x_district_coded['roles'] == 'verified,nonmember'
+    x_district_unverified_nonmembers = x_district_coded['roles'] == 'unverified,nonmember'
+    x_district_nonmembers = x_district_coded['roles'] == 'nonmember'
+    xd_nm_v = x_district_coded[x_district_nonmembers_verified]
+    xd_v_nm = x_district_coded[x_district_verified_nonmembers]
+    xd_uv_nm = x_district_coded[x_district_unverified_nonmembers]
+    xd_nm = x_district_coded[x_district_nonmembers]
+    xd_nm_total = xd_nm_v.shape[0] + xd_v_nm.shape[0] + xd_uv_nm.shape[0] + xd_nm.shape[0]
+    json_object = {}
+    keys = ['x_district_subscribers_count', 'x_district_total_members', 'xd_uv_count', 'xd_nm_total']
+    list_totals = [x_district_subscribers_count, x_district_total_members, xd_uv_count, xd_nm_total]
+    for key in keys: 
+        for value in list_totals: 
+            json_object[key] = value 
+            list_totals.remove(value) 
+            break
+    df = pd.DataFrame([json_object])
+    return df.to_json(orient='records')
+    
+    
+
+@app.route("/cd_2")
+def get_cd2_counts():
+    # Total Subscribers first
+    x_district = coded_df['cd'] == 2
+    x_district_coded = coded_df[x_district]
+    x_district_subscribers_count = coded_df[x_district].shape[0]
+    # Next, do Total Members
+    x_district_members_verified_count = x_district_coded['roles'] == 'member,verified'
+    x_district_verified_members_count = x_district_coded['roles'] == 'verified,member'
+    x_district_m_v = x_district_coded[x_district_members_verified_count]
+    x_district_v_m = x_district_coded[x_district_verified_members_count]
+    x_district_total_members = x_district_m_v.shape[0] + x_district_v_m.shape[0]
+    # Then, do Unverifieds
+    x_district_unverified = x_district_coded['verified'] == 'False'
+    xd_uv_count = x_district_coded[x_district_unverified].shape[0]
+    # xd_total_unverifieds = xd_uv.shape[0] + xd_uv_m.shape[0] + xd_m_uv.shape[0] + xd_uv_nm.shape[0] 
+    # Last, do Nonmembers
+    x_district_nonmembers_verified = x_district_coded['roles'] == 'nonmember,verified'
+    x_district_verified_nonmembers = x_district_coded['roles'] == 'verified,nonmember'
+    x_district_unverified_nonmembers = x_district_coded['roles'] == 'unverified,nonmember'
+    x_district_nonmembers = x_district_coded['roles'] == 'nonmember'
+    xd_nm_v = x_district_coded[x_district_nonmembers_verified]
+    xd_v_nm = x_district_coded[x_district_verified_nonmembers]
+    xd_uv_nm = x_district_coded[x_district_unverified_nonmembers]
+    xd_nm = x_district_coded[x_district_nonmembers]
+    xd_nm_total = xd_nm_v.shape[0] + xd_v_nm.shape[0] + xd_uv_nm.shape[0] + xd_nm.shape[0]
+    json_object = {}
+    keys = ['x_district_subscribers_count', 'x_district_total_members', 'xd_uv_count', 'xd_nm_total']
+    list_totals = [x_district_subscribers_count, x_district_total_members, xd_uv_count, xd_nm_total]
+    for key in keys: 
+        for value in list_totals: 
+            json_object[key] = value 
+            list_totals.remove(value) 
+            break
+    df = pd.DataFrame([json_object])
+    return df.to_json(orient='records')
+    
+    
+
+@app.route("/cd_3")
+def get_cd3_counts():
+    # Total Subscribers first
+    x_district = coded_df['cd'] == 3
+    x_district_coded = coded_df[x_district]
+    x_district_subscribers_count = coded_df[x_district].shape[0]
+    # Next, do Total Members
+    x_district_members_verified_count = x_district_coded['roles'] == 'member,verified'
+    x_district_verified_members_count = x_district_coded['roles'] == 'verified,member'
+    x_district_m_v = x_district_coded[x_district_members_verified_count]
+    x_district_v_m = x_district_coded[x_district_verified_members_count]
+    x_district_total_members = x_district_m_v.shape[0] + x_district_v_m.shape[0]
+    # Then, do Unverifieds
+    x_district_unverified = x_district_coded['verified'] == 'False'
+    xd_uv_count = x_district_coded[x_district_unverified].shape[0]
+    # xd_total_unverifieds = xd_uv.shape[0] + xd_uv_m.shape[0] + xd_m_uv.shape[0] + xd_uv_nm.shape[0] 
+    # Last, do Nonmembers
+    x_district_nonmembers_verified = x_district_coded['roles'] == 'nonmember,verified'
+    x_district_verified_nonmembers = x_district_coded['roles'] == 'verified,nonmember'
+    x_district_unverified_nonmembers = x_district_coded['roles'] == 'unverified,nonmember'
+    x_district_nonmembers = x_district_coded['roles'] == 'nonmember'
+    xd_nm_v = x_district_coded[x_district_nonmembers_verified]
+    xd_v_nm = x_district_coded[x_district_verified_nonmembers]
+    xd_uv_nm = x_district_coded[x_district_unverified_nonmembers]
+    xd_nm = x_district_coded[x_district_nonmembers]
+    xd_nm_total = xd_nm_v.shape[0] + xd_v_nm.shape[0] + xd_uv_nm.shape[0] + xd_nm.shape[0]
+    json_object = {}
+    keys = ['x_district_subscribers_count', 'x_district_total_members', 'xd_uv_count', 'xd_nm_total']
+    list_totals = [x_district_subscribers_count, x_district_total_members, xd_uv_count, xd_nm_total]
+    for key in keys: 
+        for value in list_totals: 
+            json_object[key] = value 
+            list_totals.remove(value) 
+            break
+    df = pd.DataFrame([json_object])
+    return df.to_json(orient='records')
+    
+    
+
+@app.route("/cd_4")
+def get_cd4_counts():
+    # Total Subscribers first
+    x_district = coded_df['cd'] == 4
+    x_district_coded = coded_df[x_district]
+    x_district_subscribers_count = coded_df[x_district].shape[0]
+    # Next, do Total Members
+    x_district_members_verified_count = x_district_coded['roles'] == 'member,verified'
+    x_district_verified_members_count = x_district_coded['roles'] == 'verified,member'
+    x_district_m_v = x_district_coded[x_district_members_verified_count]
+    x_district_v_m = x_district_coded[x_district_verified_members_count]
+    x_district_total_members = x_district_m_v.shape[0] + x_district_v_m.shape[0]
+    # Then, do Unverifieds
+    x_district_unverified = x_district_coded['verified'] == 'False'
+    xd_uv_count = x_district_coded[x_district_unverified].shape[0]
+    # xd_total_unverifieds = xd_uv.shape[0] + xd_uv_m.shape[0] + xd_m_uv.shape[0] + xd_uv_nm.shape[0] 
+    # Last, do Nonmembers
+    x_district_nonmembers_verified = x_district_coded['roles'] == 'nonmember,verified'
+    x_district_verified_nonmembers = x_district_coded['roles'] == 'verified,nonmember'
+    x_district_unverified_nonmembers = x_district_coded['roles'] == 'unverified,nonmember'
+    x_district_nonmembers = x_district_coded['roles'] == 'nonmember'
+    xd_nm_v = x_district_coded[x_district_nonmembers_verified]
+    xd_v_nm = x_district_coded[x_district_verified_nonmembers]
+    xd_uv_nm = x_district_coded[x_district_unverified_nonmembers]
+    xd_nm = x_district_coded[x_district_nonmembers]
+    xd_nm_total = xd_nm_v.shape[0] + xd_v_nm.shape[0] + xd_uv_nm.shape[0] + xd_nm.shape[0]
+    json_object = {}
+    keys = ['x_district_subscribers_count', 'x_district_total_members', 'xd_uv_count', 'xd_nm_total']
+    list_totals = [x_district_subscribers_count, x_district_total_members, xd_uv_count, xd_nm_total]
+    for key in keys: 
+        for value in list_totals: 
+            json_object[key] = value 
+            list_totals.remove(value) 
+            break
+    df = pd.DataFrame([json_object])
+    return df.to_json(orient='records')
+    
+    
+
+@app.route("/cd_5")
+def get_cd5_counts():
+    # Total Subscribers first
+    x_district = coded_df['cd'] == 5
+    x_district_coded = coded_df[x_district]
+    x_district_subscribers_count = coded_df[x_district].shape[0]
+    # Next, do Total Members
+    x_district_members_verified_count = x_district_coded['roles'] == 'member,verified'
+    x_district_verified_members_count = x_district_coded['roles'] == 'verified,member'
+    x_district_m_v = x_district_coded[x_district_members_verified_count]
+    x_district_v_m = x_district_coded[x_district_verified_members_count]
+    x_district_total_members = x_district_m_v.shape[0] + x_district_v_m.shape[0]
+    # Then, do Unverifieds
+    x_district_unverified = x_district_coded['verified'] == 'False'
+    xd_uv_count = x_district_coded[x_district_unverified].shape[0]
+    # xd_total_unverifieds = xd_uv.shape[0] + xd_uv_m.shape[0] + xd_m_uv.shape[0] + xd_uv_nm.shape[0] 
+    # Last, do Nonmembers
+    x_district_nonmembers_verified = x_district_coded['roles'] == 'nonmember,verified'
+    x_district_verified_nonmembers = x_district_coded['roles'] == 'verified,nonmember'
+    x_district_unverified_nonmembers = x_district_coded['roles'] == 'unverified,nonmember'
+    x_district_nonmembers = x_district_coded['roles'] == 'nonmember'
+    xd_nm_v = x_district_coded[x_district_nonmembers_verified]
+    xd_v_nm = x_district_coded[x_district_verified_nonmembers]
+    xd_uv_nm = x_district_coded[x_district_unverified_nonmembers]
+    xd_nm = x_district_coded[x_district_nonmembers]
+    xd_nm_total = xd_nm_v.shape[0] + xd_v_nm.shape[0] + xd_uv_nm.shape[0] + xd_nm.shape[0]
+    json_object = {}
+    keys = ['x_district_subscribers_count', 'x_district_total_members', 'xd_uv_count', 'xd_nm_total']
+    list_totals = [x_district_subscribers_count, x_district_total_members, xd_uv_count, xd_nm_total]
+    for key in keys: 
+        for value in list_totals: 
+            json_object[key] = value 
+            list_totals.remove(value) 
+            break
+    df = pd.DataFrame([json_object])
+    return df.to_json(orient='records')
+    
+    
+
+@app.route("/cd_6")
+def get_cd6_counts():
+    # Total Subscribers first
+    x_district = coded_df['cd'] == 6
+    x_district_coded = coded_df[x_district]
+    x_district_subscribers_count = coded_df[x_district].shape[0]
+    # Next, do Total Members
+    x_district_members_verified_count = x_district_coded['roles'] == 'member,verified'
+    x_district_verified_members_count = x_district_coded['roles'] == 'verified,member'
+    x_district_m_v = x_district_coded[x_district_members_verified_count]
+    x_district_v_m = x_district_coded[x_district_verified_members_count]
+    x_district_total_members = x_district_m_v.shape[0] + x_district_v_m.shape[0]
+    # Then, do Unverifieds
+    x_district_unverified = x_district_coded['verified'] == 'False'
+    xd_uv_count = x_district_coded[x_district_unverified].shape[0]
+    # xd_total_unverifieds = xd_uv.shape[0] + xd_uv_m.shape[0] + xd_m_uv.shape[0] + xd_uv_nm.shape[0] 
+    # Last, do Nonmembers
+    x_district_nonmembers_verified = x_district_coded['roles'] == 'nonmember,verified'
+    x_district_verified_nonmembers = x_district_coded['roles'] == 'verified,nonmember'
+    x_district_unverified_nonmembers = x_district_coded['roles'] == 'unverified,nonmember'
+    x_district_nonmembers = x_district_coded['roles'] == 'nonmember'
+    xd_nm_v = x_district_coded[x_district_nonmembers_verified]
+    xd_v_nm = x_district_coded[x_district_verified_nonmembers]
+    xd_uv_nm = x_district_coded[x_district_unverified_nonmembers]
+    xd_nm = x_district_coded[x_district_nonmembers]
+    xd_nm_total = xd_nm_v.shape[0] + xd_v_nm.shape[0] + xd_uv_nm.shape[0] + xd_nm.shape[0]
+    json_object = {}
+    keys = ['x_district_subscribers_count', 'x_district_total_members', 'xd_uv_count', 'xd_nm_total']
+    list_totals = [x_district_subscribers_count, x_district_total_members, xd_uv_count, xd_nm_total]
+    for key in keys: 
+        for value in list_totals: 
+            json_object[key] = value 
+            list_totals.remove(value) 
+            break
+    df = pd.DataFrame([json_object])
+    return df.to_json(orient='records')
+    
+    
+
+@app.route("/cd_7")
+def get_cd7_counts():
+    # Total Subscribers first
+    x_district = coded_df['cd'] == 7
+    x_district_coded = coded_df[x_district]
+    x_district_subscribers_count = coded_df[x_district].shape[0]
+    # Next, do Total Members
+    x_district_members_verified_count = x_district_coded['roles'] == 'member,verified'
+    x_district_verified_members_count = x_district_coded['roles'] == 'verified,member'
+    x_district_m_v = x_district_coded[x_district_members_verified_count]
+    x_district_v_m = x_district_coded[x_district_verified_members_count]
+    x_district_total_members = x_district_m_v.shape[0] + x_district_v_m.shape[0]
+    # Then, do Unverifieds
+    x_district_unverified = x_district_coded['verified'] == 'False'
+    xd_uv_count = x_district_coded[x_district_unverified].shape[0]
+    # xd_total_unverifieds = xd_uv.shape[0] + xd_uv_m.shape[0] + xd_m_uv.shape[0] + xd_uv_nm.shape[0] 
+    # Last, do Nonmembers
+    x_district_nonmembers_verified = x_district_coded['roles'] == 'nonmember,verified'
+    x_district_verified_nonmembers = x_district_coded['roles'] == 'verified,nonmember'
+    x_district_unverified_nonmembers = x_district_coded['roles'] == 'unverified,nonmember'
+    x_district_nonmembers = x_district_coded['roles'] == 'nonmember'
+    xd_nm_v = x_district_coded[x_district_nonmembers_verified]
+    xd_v_nm = x_district_coded[x_district_verified_nonmembers]
+    xd_uv_nm = x_district_coded[x_district_unverified_nonmembers]
+    xd_nm = x_district_coded[x_district_nonmembers]
+    xd_nm_total = xd_nm_v.shape[0] + xd_v_nm.shape[0] + xd_uv_nm.shape[0] + xd_nm.shape[0]
+    json_object = {}
+    keys = ['x_district_subscribers_count', 'x_district_total_members', 'xd_uv_count', 'xd_nm_total']
+    list_totals = [x_district_subscribers_count, x_district_total_members, xd_uv_count, xd_nm_total]
+    for key in keys: 
+        for value in list_totals: 
+            json_object[key] = value 
+            list_totals.remove(value) 
+            break
+    df = pd.DataFrame([json_object])
+    return df.to_json(orient='records')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/logic.js
@@ -0,0 +1,223 @@
+// Map layers
+var lightMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.light",
+    accessToken: API_KEY
+  })
+
+var darkMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+})
+
+var streetMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+})
+
+// geoMap styles
+var mapStyle = {
+    color: "#be1e2e",
+    fillColor: "#2f4754",
+    fillOpacity: 0.45,
+    weight: 1.5
+  };
+
+var mapStyleTwo = {
+    color: "#2f4754",
+    fillColor: "#be1e2e",
+    fillOpacity: 0.45,
+    weight: 1.5
+  };
+
+// Geo layers 
+var countyGeoLayer = new L.GeoJSON(countyGeoData, {
+  style: mapStyleTwo,
+  onEachFeature: function(feature, layer) {
+    layer.on({
+      mouseover: function(event) {
+        layer = event.target;
+        layer.setStyle({
+          fillColor: "#2f4754",
+          fillOpacity: 0.7
+        });
+      },
+      mouseout: function(event) {
+        layer = event.target;
+        layer.setStyle({
+          fillColor: "#be1e2e",
+          fillOpacity: 0.45
+        });
+      },
+      click: function(event) {
+        map.fitBounds(event.target.getBounds());
+      }
+    });
+    layer.bindPopup("<hr> <h2>" + feature.properties.county +" COUNTY" + "</h2> <hr> <h3>" + "Population: " + feature.properties.pop_2010 + "</h3>")
+  }
+});   
+
+
+var totalSubscribers = [];
+var totalMembers = [];
+var totalUnverified = [];
+var totalNonMembers = [];
+
+var congressionalGeoLayer = new L.GeoJSON(stateCongressionalDistricts, {
+  style: mapStyleTwo,
+  onEachFeature: function(feature, layer) {
+    // var totalSubscribers = [];
+    // var totalMembers = [];
+    // var totalUnverified = [];
+    // var totalNonMembers = []
+    layer.on({
+      mouseover: function(event) {
+        layer = event.target;
+        layer.setStyle({
+          fillColor: "#2f4754",
+          fillOpacity: 0.7
+        });
+      },
+      mouseout: function(event) {
+        layer = event.target;
+        layer.setStyle({
+          fillColor: "#be1e2e",
+          fillOpacity: 0.45
+        });
+      },
+      click: function(event) {
+        d3.json(`/cd_${feature.properties.FID}`, function(countData) { 
+          for (var i = 0; i < countData.length; i++) {
+            console.log(countData);
+          // create a reference variable for each object
+          var count = countData[i];
+          var totalSubs = count.x_district_subscribers_count;
+          var totalMems = count.x_district_total_members;
+          var totalUnverifieds = count.xd_uv_count;
+          var totalNonMems = count.xd_nm_total;
+          totalSubscribers.push(totalSubs);
+          totalMembers.push(totalMems);
+          totalUnverified.push(totalUnverifieds);
+          totalNonMembers.push(totalNonMems)
+          }})
+        // map.fitBounds(event.target.getBounds())
+      },
+    });
+    layer.bindPopup("<hr> <h2>" + "Congressional District " + feature.properties.FID + "</h2> <hr> <h2>" + "Total Subscribers :" + totalSubscribers[totalSubscribers.length-1] + "</h2>")
+  }
+});
+
+var stateHouseGeoLayer = new L.GeoJSON(stateHouseDistricts, {
+  style: mapStyleTwo,
+  onEachFeature: function(feature, layer) {
+    layer.on({
+      mouseover: function(event) {
+        layer = event.target;
+        layer.setStyle({
+          fillColor: "#2f4754",
+          fillOpacity: 0.7
+        });
+      },
+      mouseout: function(event) {
+        layer = event.target;
+        layer.setStyle({
+          fillColor: "#be1e2e",
+          fillOpacity: 0.45
+        });
+      },
+      click: function(event) {
+        map.fitBounds(event.target.getBounds());
+      }
+    });
+    layer.bindPopup("<hr> <h2>" + "State House District " + feature.properties.FID + "</h2> <hr>")
+  }
+});
+
+var stateSenateGeoLayer = new L.GeoJSON(stateSenateDistricts, {
+  style: mapStyleTwo,
+  onEachFeature: function(feature, layer) {
+    layer.on({
+      mouseover: function(event) {
+        layer = event.target;
+        layer.setStyle({
+          fillColor: "#2f4754",
+          fillOpacity: 0.7
+        });
+      },
+      mouseout: function(event) {
+        layer = event.target;
+        layer.setStyle({
+          fillColor: "#be1e2e",
+          fillOpacity: 0.45
+        });
+      },
+      click: function(event) {
+        map.fitBounds(event.target.getBounds());
+      }
+    });
+    layer.bindPopup("<hr> <h2>" + "State Senate District " + feature.properties.FID + "</h2> <hr>")
+  }
+});
+
+// Marker object
+var userMarkers= [];
+
+// User Icon
+var userIcon = L.icon({
+  iconUrl: 'static/CaucusRoomUserMarker.png',
+  iconSize: [16,16], // size of the icon
+  iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
+  // popupAnchor:  [0, -20] // point from which the popup should open relative to the iconAnchor
+});
+
+// User Layer object
+var userLayer = L.layerGroup();
+
+// Function to call geocoordinates into users object
+
+d3.json('/user_data', function(userData){
+  for (var i = 0; i < userData.length; i++) {
+    // create a reference variable for each object
+    var user = userData[i];
+    if (
+      user.latitude && user.longitude
+    ){
+      userMarkers.push(
+          L.marker([user.latitude, user.longitude], { icon: userIcon})
+          .bindPopup('<h5>' + 'Role: ' + user.roles + '</h5>' + '<h5>' + 'County: ' + user.county + '</h5>' +'<h5>' + 'Congressional District: ' + user.cd + '</h5>' + '<h5>' + 'State Senate District: ' + user.sd + '</h5>' + '<h5>' + 'State House District: ' + user.hd + '</h5>')
+          .addTo(userLayer))
+        // userLayer = L.layerGroup(userMarkers);
+}}});
+
+
+// baseMap & overlayMap
+var baseMaps = {
+  "Light": lightMap,
+  // "Dark": darkMap,
+  "Streets": streetMap
+}
+
+var overlayMaps = {
+  "Counties": countyGeoLayer,
+  "Congressional Districts": congressionalGeoLayer,
+  "State House Districts": stateHouseGeoLayer,
+  "State Senate Districts": stateSenateGeoLayer,
+  "Users": userLayer
+}
+
+// Call map
+var map = L.map("map", {
+  center: colorado,
+  zoom: 7,
+  maxZoom: 18,
+  minZoom: 1,
+  layers:[streetMap, congressionalGeoLayer, userLayer]
+});
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
